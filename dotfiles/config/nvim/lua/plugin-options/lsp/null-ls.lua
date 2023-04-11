14d63df348d5971c53c5aab8214a1217b9695745
khunbai-dev <khunbai.gaming@gmail.com>
2023-04-11 14:39:50 +0700

    [main] update nvim config

--- dotfiles/config/nvim/lua/plugin-options/lsp/null-ls.lua
+++ dotfiles/config/nvim/lua/plugin-options/lsp/null-ls.lua
@@ -11,9 +11,21 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+		-- diagnostics.flake8
 	},
 })
+
+-- All these packages in below list are installed using different package installer
+-- for each different language such as `prettier` installed using `NPM`
+--   [prettier] is javascript formating
+--   [stylua] is lua formatting
+--   [black] is python formatting
+--   [flake8] is python diagnostics or linter (linter and diagnostic are pretty the same thing)
+
+-- To see active source of current file us command `:NullLsInfo`
+-- To see LSP client attached to current buffer use command `:LspInfo`
